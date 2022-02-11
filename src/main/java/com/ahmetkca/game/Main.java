270f270c0759c9cc975ba603dbeba59e9d2046e8
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-02-11 11:55:31 -0500

    experimenting with new game-manager

--- src/main/java/com/ahmetkca/game/Main.java
+++ src/main/java/com/ahmetkca/game/Main.java
@@ -3,10 +3,12 @@ package com.ahmetkca.game;
 import com.ahmetkca.engine.Game;
 import com.ahmetkca.engine.GameContainer;
 
+import java.awt.event.KeyEvent;
+
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new GameManager();
+        Game game = new NewGameManager();
         GameContainer gc = new GameContainer(game);
         gc.setWIDTH(320);
         gc.setHEIGHT(240);
