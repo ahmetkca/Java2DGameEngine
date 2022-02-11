a9920e470adcf3aec65f752d11aca69e6cc65f17
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-02-11 11:35:02 -0500

    added init abstract function to be able to initialize game before entering into game loop which quite handful for setting up specific game-container settings. Also added base set-subject function to set subject for game instance

--- src/main/java/com/ahmetkca/engine/Game.java
+++ src/main/java/com/ahmetkca/engine/Game.java
@@ -1,7 +1,14 @@
 package com.ahmetkca.engine;
 
-public abstract class Game {
+import com.ahmetkca.utils.Observer;
+import com.ahmetkca.utils.Subject;
 
+public abstract class Game extends Observer{
+
+    public abstract void init(GameContainer gc);
     public abstract void update(GameContainer gc, float dt);
     public abstract void render(GameContainer gc, Renderer renderer);
+    public void setSub(Subject subject) {
+        super.setSubject(subject);
+    }
 }
