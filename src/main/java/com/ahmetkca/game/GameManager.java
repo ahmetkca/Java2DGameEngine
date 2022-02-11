2141a3428e90279b87a6890d3e4f9b1983c7bf39
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-02-11 11:51:20 -0500

    experimenting with subject/observer game-manager as observer Input as subject

--- src/main/java/com/ahmetkca/game/GameManager.java
+++ src/main/java/com/ahmetkca/game/GameManager.java
@@ -7,6 +7,7 @@ import com.ahmetkca.engine.audio.SoundClip;
 import com.ahmetkca.engine.gfx.Image;
 import com.ahmetkca.engine.gfx.ImageTile;
 import com.ahmetkca.engine.gfx.Light;
+import com.ahmetkca.utils.Subject;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -26,11 +27,18 @@ public class GameManager extends Game {
     private Image yetAnotherTestImage;
     private Light light;
 
+    private String testInputString = "";
+
+    public GameManager(Subject subject) {
+        this.subject = subject;
+        this.subject.add(this);
+    }
+
     public GameManager() {
         woody = new Image("/woody.png");
         background = new Image("/background.png");
 //        light = new Image("/light.png");
-        light = new Light(240/2, 0xff00ffff);
+        light = new Light(240/2, 0xffffffff);
         test_me = new Image("/opaque_blue_rect.png");
         img= new Image("/test_abc.png");
         redRect = new Image("/red_rect.png");
@@ -48,6 +56,13 @@ public class GameManager extends Game {
         woody.setLightBlock(Light.FULL);
 
     }
+    float temp = 0;
+
+    @Override
+    public void init(GameContainer gc) {
+
+    }
+
     @Override
     public void update(GameContainer gc, float dt) {
         if (gc.getInput().isKeyDown(KeyEvent.VK_A)) {
@@ -56,18 +71,25 @@ public class GameManager extends Game {
         }
         if (gc.getInput().isButtonUp(MouseEvent.BUTTON1))
             System.out.println("Left mouse button is UP");
-        temp += dt * 10;
-        if (temp > 4)
+        temp += dt * 200;
+//        System.out.println(temp);
+
+        if (temp > 150) {
+            light.setColor((0xff << 24) + ((int)(Math.random() * 255) << 16)
+                    + ((int)(Math.random() * 255) << 8)
+                    + (int)(Math.random() * 255));
+            light.setupLightMap();
             temp = 0;
+        }
     }
 
-    float temp = 0;
+
     @Override
     public void render(GameContainer gc, Renderer renderer) {
 //        renderer.drawImage(light, 0, 0);
         renderer.fillRectangle(75, 75, 0,0, (((int)(0.15*255))<<24)+0xff0000);
 
-        renderer.setzDepth(0);
+//        renderer.setzDepth(0);
         renderer.drawImage(background, 0, 0);
         renderer.drawImage(woody,
                 (gc.getWIDTH()/2) - (woody.getW()/2),
@@ -79,6 +101,9 @@ public class GameManager extends Game {
         renderer.drawLight(light,
                 gc.getInput().getMouseX(),
                 gc.getInput().getMouseY());
+        renderer.drawText(testInputString, 25, 25, 0xffffffff);
+
+//        renderer.fillRectangle(0, 0, 100, 100, 0xff000000);
 
 //        renderer.drawImageTile((ImageTile) testImageAlphaBlending,
 //                gc.getInput().getMouseX() - (testImageAlphaBlending.getW() / 2),
@@ -93,13 +118,24 @@ public class GameManager extends Game {
 //        renderer.setzDepth(3);
 //        renderer.drawImage(img , (gc.getWIDTH()/4), (gc.getHEIGHT()/4) - (blueRect.getH()/2));
 
-        renderer.drawLine(0, gc.getHEIGHT(), gc.getWIDTH(), 0, 0x0f0f0f);
 
-        renderer.drawLine(25, gc.getHEIGHT(), 55, 99, 0xaaaaaa);
 
 //        renderer.drawImage(test_me , (gc.getWIDTH()/2), (gc.getHEIGHT()/2) - (blueRect.getH()/2));
 
 //        renderer.drawImage(yetAnotherTestImage, 200, 50);
 
+    }
+
+    @Override
+    public void updateObs() {
+//        if (subject.getState() == KeyEvent.)
+        if (subject.getState() == KeyEvent.VK_BACK_SPACE) {
+            if (testInputString.length() != 0)
+                testInputString = testInputString.substring(0, testInputString.length()-1);
+        } else {
+            testInputString += (char) subject.getState();
+        }
+
+        System.out.println("Get state: " + ((char) subject.getState()));
     }
 }
